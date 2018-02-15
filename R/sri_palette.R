e83922daa07a2623898c2ff3181ca0d950366534
Kristen West <kristen.west@utah.edu>
2018-02-15 12:54:24 -0700

    first commit

---
+++ R/sri_palette.R
@@ -0,0 +1,67 @@
+#' list of custom SRI color palettes
+#'
+#' Use \code{\link{sri_palettes}} to construct palettes of desired length.
+#'
+#' @export
+
+sri_palettes <- list(
+  sricool = c("#ECF36C", "#A3DC75", "#66C081", "#38A187", "#288181", "306070", "#344154", "#2D2534"),
+  srisunset = c("#ecbc6c", "#cf5a5c", "#af5578", "#8f4c7a", "#6d416b", "#41324d"),
+  srioranges = c("#FFC79D","#F0A472","#DB834E","#C1652F","#A34915","#833100"),
+  sriblues = c("#032F3A","#13424F","#255664","#3A6B78","#50808C","#69959F","#83AAB2","#A0C0C3","#BDD6D4","#DDECE4"),
+  sricolors = c("#034d78", "#7c332a","#2a7c4d","#442c7c","#7c4c2c"),
+  sricolors2 = c("#034d78", "#7c332a","#576160","#7c2c3c","#442c7c")
+)
+
+#' An SRI palette generator
+#'
+#' These are custom SRI color palettes
+#'
+#' @param n Number of colors desired.
+#'   If omitted, uses all colours.
+#' @param name Name of desired palette. Choices are:
+#'   \code{sricolors}, \code{sricolors2},  \code{sricool},
+#'   \code{srisunset}, \code{srioranges},  \code{sriblues}
+#' @param type Either "continuous" or "discrete". Use continuous if you want
+#'   to automatically interpolate between colours.
+#'   @importFrom graphics rgb rect par image text
+#' @return A vector of colours.
+#' @export
+#' @keywords colors
+#' @examples
+#' sri_palette("sricolors")
+#' sri_palette("sricool")
+#' sri_palette("srisunset")
+#'
+#'ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
+#'geom_point(size = 3) +
+#'scale_color_manual(values = sri_palette("sricolors"))
+#'
+#'
+#' # If you need more colours than normally found in a palette, you
+#' # can use a continuous palette to interpolate between existing
+#' # colours
+#' pal <- sri_palette(12, name = "sricool", type = "continuous")
+#' image(volcano, col = pal)
+
+sri_palette <- function(name, n, type = c("discrete", "continuous")) {
+  type <- match.arg(type)
+
+  pal <- sri_palettes[[name]]
+  if (is.null(pal))
+    stop("Palette not found.")
+
+  if (missing(n)) {
+    n <- length(pal)
+  }
+
+  if (type == "discrete" && n > length(pal)) {
+    stop("Number of requested colors greater than what palette can offer")
+  }
+
+  out <- switch(type,
+                continuous = grDevices::colorRampPalette(pal)(n),
+                discrete = pal[1:n]
+  )
+  structure(out, class = "palette", name = name)
+}
---
+++ R/theme_sri.R
@@ -0,0 +1,51 @@
+#' SRI theme for plots
+#'
+#' This is a modification of theme_minimal
+#' @param font Helvetica
+#' @keywords SRI theme
+#' @export
+#' @examples
+#' Theme_sri()
+
+Theme_SRI <- function(base_size=11, font="Helvetica"){
+  titletxt <- ggplot2::element_text(size=base_size-2, colour = "#404040", hjust = 0.5)
+  subtitletxt <- ggplot2::element_text(size=base_size-3, colour = "#404040", hjust = 0.5)
+  txt2 <- ggplot2::element_text(size=base_size-3, colour = "#404040")
+
+  ggplot2::theme_minimal(base_size = base_size, base_family = font)+
+
+    #specify font sizes
+    ggplot2::theme(
+      text = txt2,
+      plot.title=titletxt,
+      plot.subtitle=subtitletxt)
+}
+
+#' SRI theme for plots
+#'
+#' This is a modification of theme_minimal
+#' @param font Helvetica
+#' @keywords SRI theme
+#' @export
+#' @examples
+#' theme_sri()
+
+theme_sri <- Theme_SRI
+
+
+
+#' SRI basic theme and colors
+#'
+#' Apply basic theme and colors to plots. This is a combination of theme_sri and sets the default color palette to "sricolors".
+#' Use for simple plots only with 1-5 qualitative colors
+#' @export
+#' @examples
+#'ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
+#'geom_point(size = 3) +
+#'sribasic
+
+
+sribasic <- list(theme_sri() , ggplot2::scale_fill_manual(values=sri_palette("sricolors")), ggplot2::scale_color_manual(values=sri_palette("sricolors")))
+
+
+
