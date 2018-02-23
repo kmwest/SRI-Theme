23c2aa8493c712fc46abc786082abf200e948f80
Kristen West <kristen.west@utah.edu>
2018-02-23 15:29:07 -0700

    Palette update

--- R/sri_palette.R
+++ R/sri_palette.R
@@ -5,7 +5,7 @@
 #' @export
 
 sri_palettes <- list(
-  sricool = c("#ECF36C", "#A3DC75", "#66C081", "#38A187", "#288181", "306070", "#344154", "#2D2534"),
+  sricool = c("#ECF36C", "#A3DC75", "#66C081", "#38A187", "#288181", "#306070", "#344154", "#2D2534"),
   srisunset = c("#ecbc6c", "#cf5a5c", "#af5578", "#8f4c7a", "#6d416b", "#41324d"),
   srioranges = c("#FFC79D","#F0A472","#DB834E","#C1652F","#A34915","#833100"),
   sriblues = c("#032F3A","#13424F","#255664","#3A6B78","#50808C","#69959F","#83AAB2","#A0C0C3","#BDD6D4","#DDECE4"),
@@ -67,4 +67,20 @@ sri_palette <- function(name, n, type = c("discrete", "continuous")) {
                 discrete = pal[1:n]
   )
   structure(out, class = "palette", name = name)
+}
+
+
+#' @export
+#' @importFrom graphics rect par image text
+#' @importFrom grDevices rgb
+print.palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1, as.matrix(1:n), col = x,
+        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
 }
